e3f4a55cff9f621b4167d58161e02093019cbe25
salimjonovsamandar <149486604+salimjonovsamandar@users.noreply.github.com>
2024-02-24 10:46:26 +0500

    Vazifa bajarildi

--- src/components/CameraComponent/index.jsx
+++ src/components/CameraComponent/index.jsx
@@ -21,18 +21,16 @@ function Index() {
         canvas.height = videoRef.current.videoHeight;
         canvas.getContext('2d').drawImage(videoRef.current, 0, 0);
 
-        // Olingan rasmni URL shaklida olish
         const imageUrl = canvas.toDataURL('image/png');
 
-        // Rasmni kompyuterga saqlash
         saveImage(imageUrl);
     };
 
     const saveImage = (imageUrl) => {
-        // Rasmni kompyuterga yuklash uchun a tag'ini yaratish
+
         const link = document.createElement('a');
         link.href = imageUrl;
-        link.download = 'captured_image.png'; // Fayl nomini qo'yish
+        link.download = 'captured_image.png';
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
